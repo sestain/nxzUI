8a2a2e5efe961051a8ce77e2ed7063d9a41c7917
n4zzu <42706009+n4zzu@users.noreply.github.com>
2020-12-28 18:58:25 +0000

    Update nxzUI.lua

--- nxzUI.lua
+++ nxzUI.lua
@@ -1,5 +1,93 @@
 local font = draw.CreateFont('Verdana', 12);
  
+function update()
+
+    local SCRIPT_FILE_NAME = GetScriptName()
+    local SCRIPT_FILE_ADDR = "https://raw.githubusercontent.com/n4zzu/nxzUI/main/nxzUI.lua"
+    local VERSION_FILE_ADDR = "https://raw.githubusercontent.com/n4zzu/nxzUI/main/version.txt"
+    local VERSION_NUMBER = "1.0"
+    local version_check_done = false
+    local update_downloaded = false
+    local update_available = false
+    local up_to_date = false
+    local updaterfont1 = draw.CreateFont("Bahnschrift", 18)
+    local updaterfont2 = draw.CreateFont("Bahnschrift", 14)
+    local updateframes = 0
+    local fadeout = 0
+    local spacing = 0
+    local fadein = 0
+
+    callbacks.Register( "Draw", "handleUpdates", function()
+        if updateframes < 5.5 then
+            if up_to_date or updateframes < 0.25 then
+                updateframes = updateframes + globals.AbsoluteFrameTime()
+                if updateframes > 5 then
+                    fadeout = ((updateframes - 5) * 510)
+                end
+                if updateframes > 0.1 and updateframes < 0.25 then
+                    fadein = (updateframes - 0.1) * 4500
+                end
+                if fadein < 0 then fadein = 0 end
+                if fadein > 650 then fadein = 650 end
+                if fadeout < 0 then fadeout = 0 end
+                if fadeout > 255 then fadeout = 255 end
+            end
+            if updateframes >= 0.25 then fadein = 650 end
+            for i = 0, 600 do
+                local alpha = 200-i/3 - fadeout
+                if alpha < 0 then alpha = 0 end
+                draw.Color(15,15,15,alpha)
+                draw.FilledRect(i - 650 + fadein, 0, i+1 - 650 + fadein, 30)
+                draw.Color(239, 150, 255,alpha)
+                draw.FilledRect(i - 650 + fadein, 30, i+1 - 650 + fadein, 31)
+            end
+            draw.SetFont(updaterfont1)
+            draw.Color(239, 150, 255,255 - fadeout)
+            draw.Text(7 - 650 + fadein, 7, "nxz")
+            draw.Color(225,225,225,255 - fadeout)
+            draw.Text(7 + draw.GetTextSize("nxz ") - 650 + fadein, 7, "UI")
+            draw.Color(239, 150, 255,255 - fadeout)
+            draw.Text(7 + draw.GetTextSize("nxzUI  ") - 650 + fadein, 7, "\\\\")
+            spacing = draw.GetTextSize("nxzUI  \\  ")
+            draw.SetFont(updaterfont2)
+            draw.Color(225,225,225,255 - fadeout)
+        end
+
+        if (update_available and not update_downloaded) then
+            draw.Text(7 + spacing - 650 + fadein, 9, "Downloading latest version.")
+            local new_version_content = http.Get(SCRIPT_FILE_ADDR);
+            local old_script = file.Open(SCRIPT_FILE_NAME, "w");
+            old_script:Write(new_version_content);
+            old_script:Close();
+            update_available = false
+            update_downloaded = true
+        end
+        
+        if (update_downloaded) then
+            draw.Text(7 + spacing - 650 + fadein, 9, "Update available, please reload the script.")
+            return
+        end
+    
+        if (not version_check_done) then
+            version_check_done = true
+            local version = http.Get(VERSION_FILE_ADDR)
+            version = string.gsub(version, "\n", "")
+            if (version ~= VERSION_NUMBER) then
+                update_available = true
+            else 
+                up_to_date = true
+            end
+        end
+        
+        if up_to_date and updateframes < 5.5 then
+            draw.Text(7 + spacing - 650 + fadein, 9, "Successfully loaded latest version: v" .. VERSION_NUMBER)
+        end
+    end)
+
+end
+
+update();
+
 callbacks.Register("Draw", function()
     if (watermark:GetValue() ~= true) then
         return
@@ -320,62 +408,6 @@ function DrawUI()
     colorPicker2 = gui.ColorPicker(colorGroup, "textCol", "Text Colour", 255,255,255,255)
     --customNameCheckBox = gui.Checkbox(mainGroup, "cNCheckBox", "Custom Watermark Name", 0)
     --editbox = gui.Editbox(mainGroup, "textBox", "Custom watermark name")
-
-    local SCRIPT_FILE_NAME = GetScriptName()
-    local SCRIPT_FILE_ADDR
-    local VERSION_FILE_ADDR
-    local VERSION_NUMBER = "1.0"
-    local version_check_done = true
-    local update_downloaded = true
-    local update_available = false
-    local up_to_date = true
-    local updaterfont1 = draw.CreateFont("Bahnschrift", 18)
-    local updaterfont2 = draw.CreateFont("Bahnschrift", 14)
-    local updateframes = 0
-    local fadeout = 0
-    local spacing = 0
-    local fadein = 0
-
-    callbacks.Register( "Draw", "handleUpdates", function()
-        if updateframes < 5.5 then
-            if up_to_date or updateframes < 0.25 then
-                updateframes = updateframes + globals.AbsoluteFrameTime()
-                if updateframes > 5 then
-                    fadeout = ((updateframes - 5) * 510)
-                end
-                if updateframes > 0.1 and updateframes < 0.25 then
-                    fadein = (updateframes - 0.1) * 4500
-                end
-                if fadein < 0 then fadein = 0 end
-                if fadein > 650 then fadein = 650 end
-                if fadeout < 0 then fadeout = 0 end
-                if fadeout > 255 then fadeout = 255 end
-            end
-            if updateframes >= 0.25 then fadein = 650 end
-            for i = 0, 600 do
-                local alpha = 200-i/3 - fadeout
-                if alpha < 0 then alpha = 0 end
-                draw.Color(15,15,15,alpha)
-                draw.FilledRect(i - 650 + fadein, 0, i+1 - 650 + fadein, 30)
-                draw.Color(239, 150, 255,alpha)
-                draw.FilledRect(i - 650 + fadein, 30, i+1 - 650 + fadein, 31)
-            end
-            draw.SetFont(updaterfont1)
-            draw.Color(239, 150, 255,255 - fadeout)
-            draw.Text(7 - 650 + fadein, 7, "nxz")
-            draw.Color(225,225,225,255 - fadeout)
-            draw.Text(7 + draw.GetTextSize("nxz ") - 650 + fadein, 7, "UI")
-            draw.Color(239, 150, 255,255 - fadeout)
-            draw.Text(7 + draw.GetTextSize("nxzUI  ") - 650 + fadein, 7, "\\\\")
-            spacing = draw.GetTextSize("nxzUI  \\  ")
-            draw.SetFont(updaterfont2)
-            draw.Color(225,225,225,255 - fadeout)
-        end
-        
-        if up_to_date and updateframes < 5.5 then
-            draw.Text(7 + spacing - 650 + fadein, 9, "Successfully loaded latest version: v" .. VERSION_NUMBER)
-        end
-    end)
 
     end
 
